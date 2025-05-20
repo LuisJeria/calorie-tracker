11853c61fc32d8821b928ce88013e0593641d7af
Andres Longton <andres.longton@congreso.cl>
2025-05-20 12:36:58 -0400

    Primer Commit: Calorie-tracker tiene persistencia de los campos del formulario

---
+++ src/components/Form.tsx
@@ -0,0 +1,72 @@
+import { useState, type ChangeEvent } from "react";
+import { categories } from "../data/categories";
+
+export default function Form() {
+  const [activity, setActivity] = useState({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value,
+    });
+    console.log(e.target);
+  };
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <div className="grid grid-cols-1 gap-3">
+        <label className="font-bold" htmlFor="category">
+          Categoría:
+        </label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label className="font-bold" htmlFor="name">
+          Actividad:
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej: Correr 30 minutos, Comer pizza, etc."
+          value={activity.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label className="font-bold" htmlFor="calories">
+          Calorias:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej: 500"
+          value={activity.calories}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value="Guardar"
+      />
+    </form>
+  );
+}
